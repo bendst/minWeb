6edb42becfb0d36f268e6669b10594f4681a3f7b
Ben Stuart <mail@bstuart.de>
2016-03-16 17:13:02 +0100

    replaced mutex with RwLock

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,28 @@ use std::fs::File;
 use std::io::prelude::{Read, Write};
 use std::io::BufReader;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::env;
 
 const USAGE: &'static str = r#"
-Starting minimal webserver.
 
 Usage: [PORT]
 
 Defaults to 8080.
+"#;
 
+const START: &'static str = r#"
+Starting minimal webserver"#;
+
+
+const OPTION: &'static str = r#"
 Commandline options:
 reload [ressource_name] - remove an ressource from the cache.
 exit - terminate the server.
 "#;
 
+#[inline(always)]
 fn default() -> Vec<u8> {
     let file = File::open("./html/index.html").expect("open failed");
     let mut file = BufReader::new(file);
@@ -31,6 +37,7 @@ fn default() -> Vec<u8> {
     buf
 }
 
+#[inline(always)]
 fn unpack(uri: &RequestUri) -> String {
     match uri {
         &RequestUri::AbsolutePath(ref path) => path.clone(),
@@ -38,6 +45,7 @@ fn unpack(uri: &RequestUri) -> String {
     }
 }
 
+#[inline(always)]
 fn get_data(path: &String) -> Vec<u8> {
     let mut data = String::from(".");
     if path != "/" {
@@ -57,8 +65,8 @@ fn get_data(path: &String) -> Vec<u8> {
     }
 }
 
-
-fn admin_input(thread_content: Arc<Mutex<HashMap<String, Vec<u8>>>>) {
+#[inline(always)]
+fn admin_input(thread_content: Arc<RwLock<HashMap<String, Vec<u8>>>>) {
     let mut line_buf = String::new();
 
     loop {
@@ -78,7 +86,7 @@ fn admin_input(thread_content: Arc<Mutex<HashMap<String, Vec<u8>>>>) {
 
         match op {
             (Some("reload"), Some(key)) => {
-                match thread_content.lock().unwrap().remove(key) {
+                match thread_content.write().unwrap().remove(key) {
                     Some(_) => println!("removed {}", key),
                     None => println!("No such asset"),
                 }
@@ -93,16 +101,21 @@ fn admin_input(thread_content: Arc<Mutex<HashMap<String, Vec<u8>>>>) {
 
 
 fn main() {
-    let content: Arc<Mutex<HashMap<String, Vec<u8>>>> = Arc::new(Mutex::new(HashMap::new()));
+    let content: Arc<RwLock<HashMap<String, Vec<u8>>>> = Arc::new(RwLock::new(HashMap::new()));
     let thread_content = content.clone();
 
     let host = match env::args().nth(1) {
-        Some(port) => "127.0.0.1:".to_string() + &port,
-        None => "127.0.0.1:8080".to_string(),
+        Some(port) => {
+            println!("{} on port {}.", START, port);
+            "0.0.0.0:".to_string() + &port
+        }
+        None => {
+            println!("{} {}", START, USAGE);
+            "0.0.0.0:8080".to_string()
+        }
     };
 
-
-    println!("{}", USAGE);
+    println!("{}", OPTION);
 
     thread::spawn(move || {
         admin_input(thread_content);
@@ -113,15 +126,15 @@ fn main() {
         .handle(move |request: Request, response: Response| {
             let key = unpack(&request.uri);
             let has_key = {
-                content.lock().unwrap().contains_key(&key)
+                content.read().unwrap().contains_key(&key)
             };
 
             let data = {
                 if has_key {
-                    content.lock().unwrap().get(&key).unwrap().clone()
+                    content.read().unwrap().get(&key).unwrap().clone()
                 } else {
                     let data = get_data(&key);
-                    content.lock().unwrap().insert(key.clone(), data.clone());
+                    content.write().unwrap().insert(key.clone(), data.clone());
                     data
                 }
             };
